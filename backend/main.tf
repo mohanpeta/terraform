88bf3d2e23f05a5ebf955e626d9860b1a8bfcb3c
mohanpeta <mohan.peta09@gmail.com>
2026-04-20 11:59:43 +0530

    backup-remotestate-added

---
+++ backend/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "practice-eks-terraform1010"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
