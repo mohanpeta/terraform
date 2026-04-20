6778b09990d3786b80a1b96212f4d017592139de
mohanpeta <mohan.peta09@gmail.com>
2026-04-20 18:09:21 +0530

    changed main file

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.97.0"
-    }
-  }
-}
 
-provider "aws" {
-  region = "ap-south-1"
-}
-
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "EKS-Practice-bucket1010"
-  lifecycle {
-    prevent_destroy = false
-  }
-  
-}
 
 
