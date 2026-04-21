0e170779afe5c446b047d8ce551afe0e48acc142
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-04-21 13:09:01 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -112,6 +112,5 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table_association" "private" {
   count          = length(var.private_subnet_cidr) 
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private_route.id
+  route_table_id = aws_route_table.private_route[count.index].id
 }
-
