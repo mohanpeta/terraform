e4005b0c7683f124c5241cbb7ddb1a060944c4ad
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-04-21 13:12:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,30 +22,30 @@ resource "aws_s3_bucket" "terraform-state" {
 terraform {
   backend "s3" {
     bucket       = "practice-eks-terraform1010"
-    key          =  "dev/terraform-state-file"           # path/to/state
+    key          = "dev/terraform-state-file" # path/to/state
     use_lockfile = true
     region       = "ap-south-1"
-    encrypt      =  true
+    encrypt      = true
   }
 }
 
 
 module "vpc" {
-  source = "./modules/vpc"
-  region = var.region
-  vpc_cidr = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
+  source              = "./modules/vpc"
+  region              = var.region
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
-  availability_zones = var.availability_zones 
+  availability_zones  = var.availability_zones
 }
 
 module "eks" {
   source = "./modules/EKS"
 
-  EKS-CLUSTER-NAME = var.EKS-CLUSTER-NAME
-  subnet_id = module.vpc.private_subnet_id
+  EKS-CLUSTER-NAME    = var.EKS-CLUSTER-NAME
+  subnet_id           = module.vpc.private_subnet_id
   EKS-Cluster-Version = var.EKS-Cluster-Version
-  node_group = var.node_group
-  region = var.region
-  vpc_id = module.vpc.vpc_id
+  node_group          = var.node_group
+  region              = var.region
+  vpc_id              = module.vpc.vpc_id
 }
