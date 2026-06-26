5bb79d4d7bd521f60661f42d5b79e2e55e3316fd
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 16:36:55 +0530

    Update locals.tf

--- backend/locals.tf
+++ backend/locals.tf
@@ -1,1 +1,10 @@
 
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+locals {
+  bucket_name = "${var.state_bucket_name}-${var.environment}-${random_string.suffix.result}"
+}
