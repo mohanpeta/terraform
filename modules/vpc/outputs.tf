24bc8f0ef7aa1c7c6b1f1b860ceeec1a5429cbbd
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 17:19:53 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,21 +1,15 @@
-# VPC ID
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.EKS_VPC.id
+  value = aws_vpc.this.id
 }
 
-
-# Public subnet Id
-output "public_subnet_id" {
-  description = "The ID of the public subnet"
-  value       = aws_subnet.public[*].id
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
 }
 
-
-# Private subnet Id
-output "private_subnet_id" {
-  description = "The ID of the private subnet"
-  value       = aws_subnet.private[*].id
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
 }
 
-
+output "nat_gateway_ids" {
+  value = aws_nat_gateway.nat[*].id
+}
