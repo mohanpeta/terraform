57b33c5b494108ec6c0eaff2502fa8ddf8efcb5a
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 17:12:43 +0530

    Update locals.tf

--- modules/vpc/locals.tf
+++ modules/vpc/locals.tf
@@ -1,8 +1,25 @@
 locals {
+
   name_prefix = "eks-${var.environment}"
 
   common_tags = {
     Environment = var.environment
     ManagedBy   = "Terraform"
+    Module      = "vpc"
   }
+
+  az_count = length(var.availability_zones)
+
+  ############################################
+  # NAT LOGIC (NO HARD CODING)
+  #
+  # Priority:
+  # 1. user-defined nat_gateway_count
+  # 2. fallback = number of AZs
+  ############################################
+  nat_count = (
+    var.nat_gateway_count != null
+    ? var.nat_gateway_count
+    : local.az_count
+  )
 }
