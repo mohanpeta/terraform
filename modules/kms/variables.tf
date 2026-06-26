7ffc79d72a79b73892acf8beb74444cf9a6f54a3
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:17:19 +0530

    Update variables.tf

--- modules/kms/variables.tf
+++ modules/kms/variables.tf
@@ -1,1 +1,22 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
 
+variable "deletion_window_in_days" {
+  description = "KMS key deletion window"
+  type        = number
+  default     = 30
+}
+
+variable "enable_key_rotation" {
+  description = "Enable automatic rotation"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Additional tags"
+  type        = map(string)
+  default     = {}
+}
