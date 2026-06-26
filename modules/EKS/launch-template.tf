f4651bd3699fed2a95e5bec5d3c0fd5e4ec2ba35
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:45:11 +0530

    Update launch-template.tf

--- modules/EKS/launch-template.tf
+++ modules/EKS/launch-template.tf
@@ -1,1 +1,56 @@
+resource "aws_launch_template" "node" {
 
+  for_each = {
+    for cluster_name, cluster in var.clusters :
+    cluster_name => cluster
+  }
+
+  name_prefix = "${var.environment}-${each.key}-lt"
+
+  ############################################
+  # Block device mapping (root volume)
+  ############################################
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size           = 50
+      volume_type           = "gp3"
+      encrypted             = true
+      delete_on_termination = true
+    }
+  }
+
+  ############################################
+  # Metadata options (security best practice)
+  ############################################
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"   # IMDSv2 enforced
+  }
+
+  ############################################
+  # Monitoring
+  ############################################
+  monitoring {
+    enabled = true
+  }
+
+  ############################################
+  # Tagging
+  ############################################
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${each.key}-node"
+      }
+    )
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
