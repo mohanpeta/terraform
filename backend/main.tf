a788ae28f1c33a4cc43de01093c0706322487bb3
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 16:37:45 +0530

    Update main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -1,30 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.97.0"
-    }
-  }
+resource "aws_s3_bucket" "state" {
+  bucket        = local.bucket_name
+  force_destroy = var.enable_force_destroy
 }
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "practice-eks-terraform1010"
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-#terraform {
-#  backend "s3" {
-#    bucket       = "practice-eks-terraform1010"
-#    key          =  "dev/terraform-state-file"           # path/to/state
-#    use_lockfile = true
-#    region       = "ap-south-1"
-#    encrypt      =  true
-#  }
-#}
 
