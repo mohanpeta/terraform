4a7324a0d62f9862c6194f4d2fd645da295df6c0
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:17:40 +0530

    Update locals.tf

--- modules/kms/locals.tf
+++ modules/kms/locals.tf
@@ -1,1 +1,15 @@
+locals {
 
+  name_prefix = "eks-${var.environment}"
+
+  common_tags = merge(
+    {
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Project     = "EKS-Platform"
+      Module      = "kms"
+    },
+    var.tags
+  )
+
+}
