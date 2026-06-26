2e85eb5bf912b98f4e3eca7557674d948ecfeb15
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 21:53:20 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,1 +1,17 @@
+output "cluster_role_arn" {
 
+  value = aws_iam_role.cluster.arn
+
+}
+
+output "node_role_arn" {
+
+  value = aws_iam_role.node.arn
+
+}
+
+output "instance_profile_name" {
+
+  value = aws_iam_instance_profile.node.name
+
+}
