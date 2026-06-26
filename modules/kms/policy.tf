952d532bedfa2fb8fd27d03b5b7e90843690e3fd
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:18:38 +0530

    Update policy.tf

--- modules/kms/policy.tf
+++ modules/kms/policy.tf
@@ -1,1 +1,25 @@
+data "aws_iam_policy_document" "kms" {
 
+  statement {
+
+    sid = "EnableRootPermissions"
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+
+    actions = [
+      "kms:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+}
