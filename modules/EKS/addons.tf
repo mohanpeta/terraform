4759029f1460ba6a1735ba97d11ea0b2bee43ad6
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:48:14 +0530

    Update addons.tf

--- modules/EKS/addons.tf
+++ modules/EKS/addons.tf
@@ -1,1 +1,25 @@
 
+resource "aws_eks_addon" "this" {
+
+  for_each = {
+    for item in local.cluster_addons :
+    "${item.cluster}-${item.addon}" => item
+  }
+
+  cluster_name = aws_eks_cluster.this[each.value.cluster].name
+
+  addon_name = each.value.addon
+
+  resolve_conflicts = "OVERWRITE"
+
+  depends_on = [
+    aws_eks_cluster.this
+  ]
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${each.value.cluster}-${each.value.addon}"
+    }
+  )
+}
