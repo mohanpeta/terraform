b0b184e94865522c60abbe1247c575ffb0f1f37a
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:54:39 +0530

    Update access.tf

--- modules/EKS/access.tf
+++ modules/EKS/access.tf
@@ -1,1 +1,29 @@
 
+resource "aws_eks_access_entry" "this" {
+
+  for_each = var.clusters
+
+  cluster_name = aws_eks_cluster.this[each.key].name
+
+  principal_arn = var.cluster_admin_role_arn
+
+  type = "STANDARD"
+
+}
+#  Attach admin policy
+
+resource "aws_eks_access_policy_association" "admin" {
+
+  for_each = var.clusters
+
+  cluster_name = aws_eks_cluster.this[each.key].name
+
+  principal_arn = var.cluster_admin_role_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+}
