cbd1964b8b0d89ce37940f41ba67066b3b02226b
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 16:36:07 +0530

    Update variables.tf

--- backend/variables.tf
+++ backend/variables.tf
@@ -1,1 +1,49 @@
 
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+
+  validation {
+    condition = contains(["dev", "qa", "stage", "prod"], var.environment)
+    error_message = "Environment must be dev, qa, stage, or prod"
+  }
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner of infrastructure"
+}
+
+variable "state_bucket_name" {
+  type        = string
+  description = "Base name for S3 terraform state bucket"
+}
+
+variable "lock_table_name" {
+  type        = string
+  description = "DynamoDB lock table name"
+}
+
+variable "enable_force_destroy" {
+  type    = bool
+  default = false
+}
+
+variable "enable_versioning" {
+  type    = bool
+  default = true
+}
+
+variable "retention_days" {
+  type    = number
+  default = 90
+}
