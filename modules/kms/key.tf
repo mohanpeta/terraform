0ca9670c62c1ff3b81ca6555643325929d81fed9
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:18:58 +0530

    Update key.tf

--- modules/kms/key.tf
+++ modules/kms/key.tf
@@ -1,1 +1,18 @@
+resource "aws_kms_key" "eks" {
 
+  description = "KMS key for EKS encryption"
+
+  deletion_window_in_days = var.deletion_window_in_days
+
+  enable_key_rotation = var.enable_key_rotation
+
+  policy = data.aws_iam_policy_document.kms.json
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name_prefix}-kms-key"
+    }
+  )
+
+}
