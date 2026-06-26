c8f25800d30ea46a7b3fd0079913d1674664c5db
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 21:18:20 +0530

    Update outputs.tf

--- modules/security-groups/outputs.tf
+++ modules/security-groups/outputs.tf
@@ -1,1 +1,11 @@
+output "cluster_security_group_id" {
+  value = aws_security_group.cluster.id
+}
 
+output "node_security_group_id" {
+  value = aws_security_group.node.id
+}
+
+output "alb_security_group_id" {
+  value = aws_security_group.alb.id
+}
