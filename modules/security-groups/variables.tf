a85fa27b6e78a97d4d27de81b699da60d06c4c1a
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 21:03:27 +0530

    Update variables.tf

--- modules/security-groups/variables.tf
+++ modules/security-groups/variables.tf
@@ -1,1 +1,26 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
 
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "allowed_api_cidrs" {
+  description = "CIDRs allowed to access the Kubernetes API"
+  type        = list(string)
+
+  default = [
+    "0.0.0.0/0"
+  ]
+}
+
+variable "tags" {
+  description = "Additional tags"
+
+  type = map(string)
+
+  default = {}
+}
