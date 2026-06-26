3a26065ee43d4264c76f85de694c898647916cd5
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 23:15:26 +0530

    Update outputs.tf

--- modules/oidc/outputs.tf
+++ modules/oidc/outputs.tf
@@ -4,3 +4,17 @@ output "oidc_provider_arns" {
     k => v.arn
   }
 }
+
+output "oidc_provider_urls" {
+  value = {
+    for k, v in aws_iam_openid_connect_provider.this :
+    k => v.url
+  }
+}
+
+output "oidc_provider_ids" {
+  value = {
+    for k, v in aws_iam_openid_connect_provider.this :
+    k => v.id
+  }
+}
