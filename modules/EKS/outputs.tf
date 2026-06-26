260cfaad37aca95291e655763d199181a6828d99
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 22:56:02 +0530

    Update outputs.tf

--- modules/EKS/outputs.tf
+++ modules/EKS/outputs.tf
@@ -1,9 +1,30 @@
-output "cluster_endpoint" {
-  description = "eks cluster end point"
-  value       =  aws_eks_cluster.main-EKS-CLUSTER.endpoint
+output "cluster_names" {
+  value = [for k, v in aws_eks_cluster.this : v.name]
 }
 
 
+output "cluster_endpoints" {
+  value = {
+    for k, v in aws_eks_cluster.this :
+    k => v.endpoint
+  }
+}
 
-
-
+output "cluster_arns" {
+  value = {
+    for k, v in aws_eks_cluster.this :
+    k => v.arn
+  }
+}
+output "oidc_provider_urls" {
+  value = {
+    for k, v in aws_eks_cluster.this :
+    k => v.identity[0].oidc[0].issuer
+  }
+}
+output "node_group_ids" {
+  value = {
+    for k, v in aws_eks_node_group.this :
+    k => v.id
+  }
+}
