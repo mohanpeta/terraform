45aa387ba8af36a0bf7f0de178d5f48d91ea85ae
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 21:04:21 +0530

    Update locals.tf

--- modules/security-groups/locals.tf
+++ modules/security-groups/locals.tf
@@ -1,1 +1,14 @@
+locals {
 
+  name_prefix = "eks-${var.environment}"
+
+  common_tags = merge(
+    {
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Module      = "security-groups"
+    },
+    var.tags
+  )
+
+}
