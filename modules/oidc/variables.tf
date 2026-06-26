a8f8446c34a0d1d03c55d25f9dd14b73c4ed4769
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-26 23:13:49 +0530

    Update variables.tf

--- modules/oidc/variables.tf
+++ modules/oidc/variables.tf
@@ -1,1 +1,13 @@
+variable "clusters" {
+  description = "Map of EKS clusters"
 
+  type = map(object({
+    oidc_issuer_url = string
+    cluster_name    = string
+  }))
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
