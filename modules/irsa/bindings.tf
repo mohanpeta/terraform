1bd69e96897aac8a44a4b85c5108511f463d5190
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 09:56:22 +0530

    Update bindings.tf

--- modules/irsa/bindings.tf
+++ modules/irsa/bindings.tf
@@ -1,1 +1,25 @@
+locals {
 
+  service_accounts = {
+
+    for role_name, role in var.irsa_roles :
+
+    role_name => {
+
+      namespace = role.namespace
+
+      service_account = role.service_account
+
+      role_arn = aws_iam_role.this[role_name].arn
+
+    }
+
+  }
+
+}
+
+output "service_account_bindings" {
+
+  value = local.service_accounts
+
+}
