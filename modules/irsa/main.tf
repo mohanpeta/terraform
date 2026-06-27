402f962aa584286e643d825ffcfc3b682d5d5687
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 09:49:49 +0530

    Update main.tf

--- modules/irsa/main.tf
+++ modules/irsa/main.tf
@@ -1,1 +1,50 @@
+resource "aws_iam_role" "this" {
 
+  for_each = var.irsa_roles
+
+  name = "${local.name_prefix}-${each.key}"
+
+  assume_role_policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+
+        Effect = "Allow"
+
+        Principal = {
+          Federated = var.oidc_provider_arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+
+          StringEquals = {
+
+            "${replace(var.oidc_provider_url, "https://", "")}:sub" =
+            "system:serviceaccount:${each.value.namespace}:${each.value.service_account}"
+
+            "${replace(var.oidc_provider_url, "https://", "")}:aud" =
+            "sts.amazonaws.com"
+
+          }
+
+        }
+
+      }
+
+    ]
+
+  })
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name_prefix}-${each.key}"
+    }
+  )
+
+}
