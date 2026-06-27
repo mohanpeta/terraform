46ed2c23c1c4299c7ead0b0ffbfe193bb562cd0b
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 09:19:39 +0530

    Update variables.tf

--- modules/irsa/variables.tf
+++ modules/irsa/variables.tf
@@ -1,1 +1,30 @@
+variable "cluster_name" {
+  type = string
+}
 
+variable "oidc_provider_arn" {
+  type = string
+}
+
+variable "oidc_provider_url" {
+  type = string
+}
+
+variable "irsa_roles" {
+
+  description = "Map of IRSA roles"
+
+  type = map(object({
+
+    namespace       = string
+    service_account = string
+    policy_arns     = list(string)
+
+  }))
+
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
