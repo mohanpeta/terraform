7e87f1df24fdf968557193b1302a92321aaf7691
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 10:10:51 +0530

    Update versions.tf

--- modules/addons/versions.tf
+++ modules/addons/versions.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_version = ">= 1.7"
 
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.97"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+  }
+}
