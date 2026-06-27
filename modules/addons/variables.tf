9996e644a535afd525e1bc748fb107a6306d9091
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 10:11:10 +0530

    Update variables.tf

--- modules/addons/variables.tf
+++ modules/addons/variables.tf
@@ -1,1 +1,44 @@
+variable "cluster_name" {
+  type = string
+}
 
+variable "cluster_endpoint" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+variable "cluster_auth_token" {
+  type = string
+  sensitive = true
+}
+
+variable "oidc_provider_arn" {
+  type = string
+}
+
+variable "irsa_role_arns" {
+  type = map(string)
+}
+
+variable "aws_managed_addons" {
+
+  type = map(object({
+
+    version = optional(string)
+
+    resolve_conflicts = optional(string, "OVERWRITE")
+
+  }))
+
+}
+
+variable "tags" {
+
+  type = map(string)
+
+  default = {}
+
+}
