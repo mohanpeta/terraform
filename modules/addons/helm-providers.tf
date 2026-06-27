5a6a1c6dbbea1e6fd9561a27adce95e974856892
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 10:11:54 +0530

    Update helm-providers.tf

--- modules/addons/helm-providers.tf
+++ modules/addons/helm-providers.tf
@@ -1,1 +1,26 @@
 
+provider "kubernetes" {
+
+  host = var.cluster_endpoint
+
+  token = var.cluster_auth_token
+
+  cluster_ca_certificate = base64decode(
+    var.cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+
+  kubernetes {
+
+    host = var.cluster_endpoint
+
+    token = var.cluster_auth_token
+
+    cluster_ca_certificate = base64decode(
+      var.cluster_ca_certificate
+    )
+  }
+
+}
