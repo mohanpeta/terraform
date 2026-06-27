620077949c8ea7e59155193758c56f1188ce569e
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 09:22:40 +0530

    Update outputs.tf

--- modules/irsa/outputs.tf
+++ modules/irsa/outputs.tf
@@ -1,1 +1,13 @@
+output "irsa_role_arns" {
+  value = {
+    for k, v in aws_iam_role.this :
+    k => v.arn
+  }
+}
 
+output "irsa_role_names" {
+  value = {
+    for k, v in aws_iam_role.this :
+    k => v.name
+  }
+}
