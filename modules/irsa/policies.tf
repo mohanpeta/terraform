3d861cb7d3eb751e003779664d87e9354d7bbb88
mohanpeta <155849170+mohanpeta@users.noreply.github.com>
2026-06-27 09:51:18 +0530

    Update policies.tf

--- modules/irsa/policies.tf
+++ modules/irsa/policies.tf
@@ -1,1 +1,22 @@
 
+locals {
+
+  policy_attachments = flatten([
+
+    for role_name, role in var.irsa_roles : [
+
+      for policy in role.policy_arns : {
+
+        role_name = role_name
+
+        policy_arn = policy
+
+        key = "${role_name}-${basename(policy)}"
+
+      }
+
+    ]
+
+  ])
+
+}
